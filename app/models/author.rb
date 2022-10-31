58974a0f05e49ecd6949e9c21618b3f6d646ec15
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-31 08:17:37 +0300

    Initialize author and add a getter method.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,38 @@
-class Author
-  attr_accessor :name
+require_relative "./magazine"
+require_relative "./article"
 
+class Author
+  attr_reader :name
+
+  # @@all_magazines =  []
+  # @@all_articles = []
 
   def initialize(name)
     @name = name
-  
   end
 
+  # def magazines= (magazine)
+  #   @@all_magazines << magazine
+  # end
 
+  # def articles =()
+  #   @@all_articles << Article
+  # end
+
+  # def articles
+  #   @@all_articles << Article
+  # end
+
+  # def magazines
+  #   @@all_magazines.uniq
+  # end
 end
+
+# author1 = Author.new("Pat")
+# author1.articles = "Mahia"
+# author1.articles = "Gor"
+# author1.magazines = "The Standard"
+# author1.magazines = "The Standard"
+
+# p author1.articles
+# p author1.magazines
