ef758360bcc85684a37581d8017212a9f4dfaf0a
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-31 08:29:55 +0300

    Add getter and setter methods, and instance variables.

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,29 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
+  # @@contributors = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  # def contributors= (author)
+  #   @@contributors << author
+  # end
+
+  # def contributors
+  #   @@contributors
+  # end
 end
+
+# m1 = Magazine.new("The Standard", "Entertainment")
+# m1.contributors = "Dave"
+# m1.contributors = "Steve"
+
+# p m1.contributors
