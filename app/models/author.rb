521fbd7f582461960387b0d141470400d501af04
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-01 11:32:41 +0300

    Add methods.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
+require_relative './article'
+
 class Author
   attr_reader :name
 
@@ -5,4 +7,23 @@ class Author
     @name = name
   end
 
+  def articles
+    Article.all.select { |obj| @name == obj.author.name ? obj : nil }
+  end
+
+  def magazines
+    Article.all.select{ |obj| @name == obj.author.name ? obj : nil}
+    .map{ |article| article.magazine }.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(author: @name, magazine: magazine, title: title )
+  end
+
+  def topic_areas
+    Article.all.select { |obj| @name == obj.author.name ? obj.magazine.category : nil}
+    .map{ |article| article.magazine.category}.uniq
+  end
+
 end
+
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,8 +1,10 @@
+require_relative "./article"
+
 class Magazine
   attr_accessor :name, :category
 
   @@all = []
-  
+
   def initialize(name:, category:)
     @name = name
     @category = category
@@ -12,4 +14,24 @@ class Magazine
   def self.all
     @@all
   end
+
+  def contributors
+  Article.all.find_all { |obj| @name == obj.magazine.name ? obj.author : nil}
+  .map{ |article| article.author} 
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |obj| name == obj.name }
+  end
+
+  def article_titles
+    Article.all.filter { |obj| @name == obj.magazine.name ? obj.title : nil }
+    .map { |article| article.title }
+  end
+
+  def contributing_authors
+    Article.all.filter { |obj| @name == obj.magazine.name ? obj.title : nil }
+    .map { |article| article.author.name }
+  end
+
 end
